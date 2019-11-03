cce013d4e64bd0d1d386a50f082db16512669e0c
Etienne Gramlich <etgramlich@gmx.de>
2019-11-03 09:44:20 +0100

    Added test to StringUtil and corrected indices

--- src/main/java/de/etgramlich/antlr/util/StringUtil.java
+++ src/main/java/de/etgramlich/antlr/util/StringUtil.java
@@ -2,9 +2,10 @@ package de.etgramlich.antlr.util;
 
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public final class StringUtil extends StringUtils {
@@ -16,13 +17,24 @@ public final class StringUtil extends StringUtils {
 
     @NotNull
     public static List<String> trimBlankLines(@NotNull final List<String> lines) {
-        final int beginIndex = lines.indexOf(lines.stream().filter(s -> !isBlank(s)).findFirst().get());
-        final int endIndex = lines.indexOf(Lists.reverse(lines).stream().filter(s -> !isBlank(s)).findFirst().get());
-        return lines.subList(beginIndex, endIndex);
+        Optional<String> begin = lines.stream().filter(s -> !isBlank(s)).findFirst();
+        Optional<String> end = Lists.reverse(lines).stream().filter(s -> !isBlank(s)).findFirst();
+
+        if (begin.isEmpty() && end.isEmpty()) { // Only blank lines
+            return Collections.emptyList();
+        }
+        final int beginIndex = lines.indexOf(begin.get());
+        final int endIndex = lines.indexOf(end.get());
+        return lines.subList(beginIndex, endIndex+1);
     }
 
     @NotNull
     public static List<String> removeDuplicates(@NotNull final List<String> lines) {
         return lines.stream().distinct().collect(Collectors.toList());
+    }
+
+    @Contract(pure = true)
+    public static boolean isAllBlank(@NotNull final Collection<String> lines) {
+        return isAllBlank(lines.toArray(new String[0]));
     }
 }
