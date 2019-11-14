d32201b63003dd608a1093a89c38922608a310c8
Etienne Gramlich <etgramlich@gmx.de>
2019-11-14 12:53:02 +0100

    Simplified LetterRange and added test

--- src/main/java/de/etgramlich/antlr/parser/type/rhstype/LetterRange.java
+++ src/main/java/de/etgramlich/antlr/parser/type/rhstype/LetterRange.java
@@ -10,35 +10,19 @@ import org.jetbrains.annotations.NotNull;
 public final class LetterRange implements BnfType, BnfElement {
     private final boolean isLetter;
     private final char beginChar, endChar;
-    private final int beginInt, endInt;
 
     public LetterRange(boolean isLetter, char begin, char end) {
         if (!isLetter && !StringUtil.isAllNumeric(begin, end)) {
             throw new IllegalArgumentException("If isLetter is false, both chars must be digits, and were: " + begin + " and " + end);
         }
         this.isLetter = isLetter;
-        if (isLetter) {
-            beginChar = begin;
-            endChar = end;
-            beginInt = endInt = 0;
-        } else {
-            beginInt = Integer.parseInt(String.valueOf(begin));
-            endInt = Integer.parseInt(String.valueOf(end));
-            beginChar = endChar = 'A';
-        }
+        beginChar = begin;
+        endChar = end;
     }
 
-    public boolean isValid(final char c) {
-        if (isLetter) {
-            return c >= beginChar && c <= endChar;
-        } else {
-            try {
-                final int value = Integer.parseInt(String.valueOf(c));
-                return value >= beginInt && value <= endInt;
-            } catch (NumberFormatException e) {
-                return false;
-            }
-        }
+    @Contract(pure = true)
+    public boolean isInRange(final char c) {
+        return c >= beginChar && c <= endChar;
     }
 
     @Contract(pure = true)
