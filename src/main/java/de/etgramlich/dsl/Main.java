4ae201e9962d6eb70f29dbe36127d99efca3983d
Etienne Gramlich <etgramlich@gmx.de>
2020-03-20 12:47:05 +0100

    Corrected command line option parsing

--- src/main/java/de/etgramlich/dsl/Main.java
+++ src/main/java/de/etgramlich/dsl/Main.java
@@ -8,9 +8,11 @@ import de.etgramlich.dsl.generator.InterfaceBuilder;
 import de.etgramlich.dsl.graph.type.BnfRuleGraph;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.apache.commons.cli.Options;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.lang3.StringUtils;
 
@@ -33,16 +35,42 @@ public final class Main {
     private static final Pattern NON_TERMINAL_PATTERN = Pattern.compile("<[a-zA-Z]+>");
 
     /**
+     * Help option to be used in own method.
+     */
+    private static final Option HELP_OPTION =
+            Option.builder("h").required(false).hasArg(false).longOpt("help").desc("Prints this help text.").build();
+
+    /**
      * Stores command line options.
      */
     private static final Options OPTIONS = new Options();
     static {
-        OPTIONS.addOption("h", "help", false, "Prints this help text.");
+        OPTIONS.addOption(HELP_OPTION);
         OPTIONS.addRequiredOption("d", "directory", true, "Target directory for generated sources");
         OPTIONS.addRequiredOption("p", "package", true, "Target package");
         OPTIONS.addRequiredOption("g", "grammar", true, "Grammar file path");
         OPTIONS.addOption("s", "sketch-graph", true, "Writes DOT graph to file");
     }
+
+    /**
+     * Save line separator for current system.
+     */
+    private static final String NEWLINE = System.lineSeparator();
+
+    /**
+     * Syntax hint for help formatter.
+     */
+    private static final String CMD_SYNTAX = "java -jar antlr-test-x.x-jar-with-dependencies.jar";
+
+    /**
+     * Header for help test.
+     */
+    private static final String HEADER = "Generate interfaces from EBNF grammar" + NEWLINE + NEWLINE;
+
+    /**
+     * Footer for help text.
+     */
+    private static final String FOOTER = NEWLINE + "Report issues at https://github.com/etgramli/AntlrTest/";
 
     /**
      * Reads grammar from file and outputs java interfaces in the corresponding package directory in the provided
@@ -55,6 +83,11 @@ public final class Main {
         final String targetPackage;
         final String graphFilePath;
         try {
+            if (testForHelp(args)) {
+                final HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp(CMD_SYNTAX, HEADER, OPTIONS, FOOTER, true);
+                return;
+            }
             final CommandLine cmd = new DefaultParser().parse(OPTIONS, args);
             targetDirectory = cmd.getOptionValue("d");
             targetPackage = cmd.getOptionValue("p");
@@ -82,6 +115,13 @@ public final class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean testForHelp(final String[] args) throws ParseException {
+        final Options options = new Options();
+        options.addOption(HELP_OPTION);
+        final CommandLine commandLine = new DefaultParser().parse(options, args, true);
+        return commandLine.hasOption(HELP_OPTION.getOpt());
     }
 
     private static String prepareGrammar(final String filepath) throws IOException {
