ce5748281587d8318da6e7ed77afcde1f357a081
Etienne Gramlich <etgramlich@gmx.de>
2020-04-24 17:54:10 +0200

    Methods to export Maps of String to String

--- src/main/java/de/etgramlich/dsl/util/CollectionUtil.java
+++ src/main/java/de/etgramlich/dsl/util/CollectionUtil.java
@@ -47,14 +47,44 @@ public final class CollectionUtil {
             throw new IllegalArgumentException("Second column must no be blank!");
         }
         final String lineSeparator = " | ";
-        final StringBuilder sb = new StringBuilder(firstColumn)
-                .append(lineSeparator)
-                .append(secondColumn)
-                .append(System.lineSeparator())
-                .append("------|------")
-                .append(System.lineSeparator());
+        final String head = firstColumn + lineSeparator + secondColumn + System.lineSeparator();
+        return head + "------|------" + System.lineSeparator() + appendToLines(map, lineSeparator, StringUtil.EMPTY);
+    }
+
+    /**
+     * Generates a LaTeX representation of the passed map as tabular with the strings as headlines for the columns.
+     * @param map Map of String to String, must not be null.
+     * @param firstColumn String, must not be blank.
+     * @param secondColumn String, must not be blank.
+     * @return String, not null.
+     */
+    public static String toLaTeXTable(final Map<String, String> map,
+                                      final String firstColumn,
+                                      final String secondColumn) {
+        if (map == null) {
+            throw new IllegalArgumentException("Map must not be null!");
+        }
+        if (StringUtil.isBlank(firstColumn)) {
+            throw new IllegalArgumentException("First column must not be blank!");
+        }
+        if (StringUtil.isBlank(secondColumn)) {
+            throw new IllegalArgumentException("Second column must no be blank!");
+        }
+        final String lineSeparator = " & ";
+        final String latexLineEnd = "\\\\";
+        final String tabular = "\\begin{tabular}{ll}" + System.lineSeparator();
+        final String headLines = "\\textbf{" + firstColumn + "}" + lineSeparator + "\\textbf{" + secondColumn + "}";
+        return tabular + headLines + latexLineEnd + "\\hline" + System.lineSeparator()
+                + appendToLines(map, lineSeparator, latexLineEnd);
+    }
+
+    private static String appendToLines(final Map<String, String> map, final String separator, final String endLine) {
+        final StringBuilder sb = new StringBuilder();
         for (Map.Entry<String, String> entry : map.entrySet()) {
-            sb.append(entry.getKey()).append(lineSeparator).append(entry.getValue()).append(System.lineSeparator());
+            sb.append(entry.getKey())
+                    .append(separator)
+                    .append(entry.getValue())
+                    .append(endLine).append(System.lineSeparator());
         }
         return sb.toString();
     }
